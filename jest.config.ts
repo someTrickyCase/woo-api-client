51dc40168014bfe9ca5f5ffad8ac23fb425b5d29
someTrickyCase <snowinboots18@gmail.com>
2026-07-15 13:30:54 +0400

    node-ts switched to tsx

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
+	testTimeout: 30000,
 	preset: "ts-jest",
 	testEnvironment: "node",
 	roots: ["<rootDir>/src"],
